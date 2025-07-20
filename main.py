4945a1d355119df73c0309e2fbedf06579d24cb9
Alco80 <leeshla2525@gmail.com>
2025-07-20 18:46:53 -0400

    Added new photo of me

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import streamlit as st
+st.set_page_config(layout='wide')
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/alicia.png")
 
 with col2:
     st.title("Alicia Corke")
     content = """
-I'm Alicia Corke and I'm a programmer.
+I'm Alicia Corke and I'm a programmer from Ottawa Ontario, Canada.
 
 """  
+
+st.info(content)
